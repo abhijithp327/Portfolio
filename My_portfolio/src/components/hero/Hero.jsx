192b96c2ceee000d92ed3cd3d4b713772804e3f6
Anurag p <87142203+abhijithp327@users.noreply.github.com>
2023-10-30 13:37:05 +0530

    Added images and designs

--- My_portfolio/src/components/hero/Hero.jsx
+++ My_portfolio/src/components/hero/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
         variants={fadeIn("up", "tween", 0.3, 1)}
         className={css.person}
       >
-        <motion.img variants={slideIn("up", "tween", 0.5, 1.3)} src="./person.png" alt="" />
+        <motion.img variants={slideIn("up", "tween", 0.5, 1.3)} src="./abhijithp.png" alt="abh" />
       </motion.div>
 
  
